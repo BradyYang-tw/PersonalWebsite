276660927a1606d5afe604a5c8602555ca14a538
HsinYu <hsinyu.yang@hwacom>
2023-03-22 00:04:43 +0800

    to develop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import _ from 'lodash';
+import Icon from './icon.png';
+import './style.css'
+import Data from './data.xml';
+import Notes from './data.csv';
+import printMe from './print.js';
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+     // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    console.log(Data);
+    console.log(Notes);
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
